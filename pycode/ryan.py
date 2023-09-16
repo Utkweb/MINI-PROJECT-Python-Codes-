51c790a45ed3237f6850c4d4bd336e9597e61388
Utkweb <us196000@gmail.com>
2023-09-17 02:19:57 +0530

    .

--- pycode/ryan.py
+++ pycode/ryan.py
@@ -63,6 +63,8 @@
 
 # if a>b:
 #     print("a is greater than b")
+#     if a%2 == 0:
+#         print("a is even")
 # elif a<b:
 #     print("a is less than b")
 # else:
@@ -102,11 +104,7 @@
 #     else:
 #         odd_sum += i
 # print("The sum of even numbers: ",even_sum)
-# print("The sum of odd numbers: ",odd_sum)
-
-# fibonacci series:-
-# 0 1 1 2 3 5 8 13 21 34 55 89 144 233 377
-    
+# print("The sum of odd numbers: ",odd_sum)    
     
 # num1 =42
 # num2 = 120
@@ -127,4 +125,104 @@
 # print("The value of num2: ",num2)   
 
 
-* //
+# 1 - 10
+
+# piggy_bank = 0
+# for i in range(1,12,2):
+#     piggy_bank += i
+# print("The sum of odd numbers: ",piggy_bank)
+
+
+# Nested Loops:-
+
+# Week 1:
+#     Day 1:-
+#     Day 2:-
+#     Day 3:-
+#     Day 4:-
+#     Day 5:-
+#     Day 6:-
+#     Day 7:-
+# Week 2:
+
+
+# for var in range(start,end,step):
+#     for var in range(start,end,step):
+#         statement1
+#         statement2
+#         statement3
+#     statement4
+#     statement5
+#     statement6
+
+# for i in range(1,4):
+#     for j in range(1,5):
+#         print(i," ",j)
+        
+# 1 1
+# 1 2
+# 1 3
+# 1 4
+# 2 1
+# 2 2
+# 2 3
+# 2 4
+# 3 1
+# 3 2
+# 3 3
+# 3 4
+
+
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5
+
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#     print()
+    
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print("*",end=" ")
+#     print()
+
+
+# 1 
+# 2 2
+# 3 3 3
+# 4 4 4 4
+# 5 5 5 5 5
+
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(i,end=" ")
+#     print()
+
+
+
+# fibonacci series:-
+# 0 1 1 2 3 5 8 13 21 34 55 89 144 233 377
+
+# num1 = 0    
+# num2 = 1
+# result = 0
+
+# print(num1," ",num2,end=" ")
+# for i in range(1,11):
+#     result = num1+num2
+#     print(result,end=" ")
+#     num1 = num2
+#     num2 = result
+
+# 123 -->  321
+# 345 -->  543
+    
